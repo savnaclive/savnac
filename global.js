af80979f278a2fff7fe07752ce540e4188804042
savnac <harrison.t135@stu.nebo.edu>
2025-11-03 11:12:52 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 // global.js
 document.addEventListener("keydown", function (e) {
   if (e.key.toLowerCase() === "q") {
-    window.location.href = "https://canvas.nebo.edu/";
+    window.location.href = "https://www.instructure.com/canvas/login";
   }
 });
